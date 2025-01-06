1450b99ce7ac0a28196e6693fbe8ec1e583d4596
tomwattsuk <46566342+tomwattsuk@users.noreply.github.com>
2025-01-06 22:48:02 +0000

    Trying a build fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,21 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { fileURLToPath } from 'url';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  },
-  optimizeDeps: {
-    exclude: ['better-sqlite3']
   }
 });
