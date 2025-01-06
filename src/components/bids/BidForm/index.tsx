14903bee476bf0e13fb36ab0b3ee6c0c7fc486e0
tomwattsuk <46566342+tomwattsuk@users.noreply.github.com>
2025-01-06 18:54:14 +0000

    new configuration

--- src/components/bids/BidForm/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { BidFormContent } from './BidFormContent';
-import { BidFormProps } from './types';
-
-export const BidForm: React.FC<BidFormProps> = (props) => {
-  return <BidFormContent {...props} />;
-};
